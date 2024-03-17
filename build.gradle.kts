b2be5bb692b78f9576549548cc3d2cccb7485203
minjun-il <alswnsdlf93@gmail.com>
2024-03-17 23:19:14 +0900

    -

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+	java
+	alias(libs.plugins.spring.boot)
+	alias(libs.plugins.spring.dependency.management)
+	id("jacoco")
+}
+
+sourceSets {
+	getByName("main").java.srcDirs("src/main/kotlin", "src/main/java")
+	getByName("test").java.srcDirs("src/test/kotlin", "src/test/java")
+}
+
+allprojects {
+	group = property("app.group").toString()
+}
+
+dependencyManagement {
+	imports {
+		mavenBom(libs.spring.cloud.dependencies.get().toString())
+	}
+}
+
+group = "io.hhplus"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+with(extensions.getByType(JacocoPluginExtension::class.java)) {
+	toolVersion = "0.8.7"
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+dependencies {
+	implementation(libs.spring.boot.starter.web)
+	annotationProcessor(libs.spring.boot.configuration.processor)
+	testImplementation(libs.spring.boot.starter.test)
+
+	compileOnly(libs.lombok)
+	annotationProcessor(libs.lombok)
+}
+
+// bundling tasks
+tasks.getByName("bootJar") {
+	enabled = true
+}
+tasks.getByName("jar") {
+	enabled = false
+}
+// test tasks
+tasks.test {
+	ignoreFailures = true
+	useJUnitPlatform()
+}
