827cd19e1104f9b804a51d6bb09bd3dafb31882a
minjun-il <alswnsdlf93@gmail.com>
2024-05-09 21:28:12 +0900

    test 작성

--- src/main/java/io/hhplus/tdd/TddApplication.java
+++ src/main/java/io/hhplus/tdd/TddApplication.java
@@ -3,7 +3,6 @@ package io.hhplus.tdd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// push test
 @SpringBootApplication
 public class TddApplication {
 	public static void main(String[] args) {
--- src/test/java/io/hhplus/tdd/TddApplicationTest.java
+++ src/test/java/io/hhplus/tdd/TddApplicationTest.java
@@ -15,45 +15,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-class TddApplicationTest {
-
-  private PointService pointService;
-
-  @BeforeEach
-  public void setUp() {
-
-    pointService = new PointService();
-  }
+@SpringBootTest
+class TddApplicationTests {
 
   @Test
-  public void testChargePoints() {
-    Point point = pointService.charge(1L, 100);
-    assertEquals(100, point.getClass());
+  void contextLoads() {
   }
 
-//  @Test
-//  public void testUsePoints() {
-//    pointService.charge(1L, 100);
-//    assertTrue(pointService.use(1L, 50));
-//    assertEquals(50, pointService.use(1L));
-//  }
-
-//  @Test
-//  public void testGetBalance() {
-//    pointService.charge(1L, 100);
-//    assertEquals(100, pointService.history(1L));
-//  }
-
-  @Test
-  public void testGetHistories() {
-    pointService.charge(1L, 100);
-    pointService.use(1L, 50);
-    assertEquals(2, pointService.histories(1L).size());
-  }
-
-  @Test
-  public void testInsufficientBalance() {
-    pointService.charge(1L, 100);
-    assertFalse(pointService.use(1L, 150));
-  }
 }
